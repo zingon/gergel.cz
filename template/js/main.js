0e46cbe98cffbf7d8548a00fe9333a29d329d1ba
Libor Hála <libor.hala@gmail.com>
2015-03-14 15:52:26 +0100

    Kontakty, konečně. :D

--- template/js/main.js
+++ template/js/main.js
@@ -2,5 +2,5 @@
  * Created by Libor on 26. 2. 2015.
  */
 $(document).ready(function() {
-    $(".row, .nav").append("<div class='cleaner'></div>");
+    $(".row, .nav, .col-sm-height>div").append("<div class='cleaner'></div>");
 });
