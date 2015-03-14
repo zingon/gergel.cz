7461d90d06eb4f7525b8ba3250e31dff9eb0ec04
Libor Hála <libor.hala@gmail.com>
2015-03-14 18:03:54 +0100

    Menu, hlavička

--- template/js/main.js
+++ template/js/main.js
@@ -1,6 +1,13 @@
 /**
  * Created by Libor on 26. 2. 2015.
  */
-$(document).ready(function() {
+
+
+$(document).ready(function main() {
     $(".row, .nav, .col-sm-height>div").append("<div class='cleaner'></div>");
+
+    $(".navToggle").click(function() {
+        $(this).toggleClass("active");
+        $("#mainNav .nav").toggle("blind", 1000);
+    });
 });
