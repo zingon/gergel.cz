a5064bf1bd842ddb393242c60836a19c23f86196
Libor Hála <libor.hala@gmail.com>
2015-03-17 20:26:37 +0100

    galerie

--- template/js/main.js
+++ template/js/main.js
@@ -11,18 +11,23 @@ $(document).ready(function main() {
         $("#mainNav .nav").toggle("blind", 1000);
     });
 
-    $('.bxslider').bxSlider({
+    $('.bxNormal').bxSlider({
         minSlides: 3,
         maxSlides: 5,
         slideWidth: 170,
         slideMargin: 5,
         pager: false
     });
-    $('.bxslider small').bxSlider({
+    $('.bxSmall').bxSlider({
         minSlides: 3,
         maxSlides: 3,
         slideWidth: 110,
-        slideMargin: 0,
+        slideMargin: 10,
         pager: false
+    });
+
+    $(".slide a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().parent().parent().parent().parent().find(".galleryCanvas").attr('src', $(this).attr("href"));
     });
 });
